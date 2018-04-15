be53f39ee0e7e9ebac541bb64937fe08288c5615
kalagnie <37547184+kalagnie@users.noreply.github.com>
2018-04-15 21:19:23 +0200

    question script

---
+++ game-lazienki/Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class QuestionManager : MonoBehaviour {
+
+	public Question[] questions;
+	private static List<Question> unansweredQuestions;
+
+	private Question currentQuestion;
+	private int currentQuestionIndex = 0;
+
+	[SerializeField]
+	private Text questionText;
+
+	[SerializeField]
+	private float TimeBetweenQuestions = 1f;
+
+	void Start(){
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+			unansweredQuestions = questions.ToList<Question>();
+
+		currentQuestion = unansweredQuestions [currentQuestionIndex];
+		questionText.text = currentQuestion.question;
+
+		//testing
+		Debug.Log (currentQuestion.question);
+	}
+
+	//void Update(){
+	//questionText.text = currentQuestion.question;
+	//}
+
+	IEnumerator TransitionToNextQuestion(){
+		unansweredQuestions.Remove (currentQuestion);
+		yield return new WaitForSeconds (TimeBetweenQuestions);
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void UserSelectAnswer1(){
+		Debug.Log ("Answer1");
+		currentQuestionIndex = 1;
+		StartCoroutine(TransitionToNextQuestion ());
+	}
+
+	public void UserSelectAnswer2(){
+		Debug.Log ("Answer2");
+		currentQuestionIndex = 0;
+		StartCoroutine(TransitionToNextQuestion ());
+	}
+}

83494af5119d43a85892b1ee641e7a67ab37c497
kalagnie <37547184+kalagnie@users.noreply.github.com>
2018-06-05 16:39:47 +0200

    Touching objects
    Raycast and rotating cubes

---
+++ game-lazienki/Assets/Scripts/Map/ZoomAndScroll.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomAndScroll : MonoBehaviour {
+
+	Camera mainCamera;
+	float touchesPrevPosDifference, touchesCurPosDifference, zoomModifier;
+	Vector2 firstTouchPrevPos, secondTouchPrevPos;
+
+	//public float zoomSpeed = 0.7f;
+	//float prevDist;
+
+	[SerializeField]
+	float zoomModifierSpeed = 0.1f;
+
+	[SerializeField]
+	Text text;
+
+	void Start(){
+		mainCamera = GetComponent<Camera> ();
+	}
+
+	void Update () {
+		if (Input.touchCount == 2) {
+
+			UnityEngine.Touch firstTouch = Input.GetTouch (0);
+			UnityEngine.Touch secondTouch = Input.GetTouch (1);
+
+			firstTouchPrevPos = firstTouch.position - firstTouch.deltaPosition;
+			secondTouchPrevPos = secondTouch.position - secondTouch.deltaPosition;
+
+			touchesPrevPosDifference = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+			touchesCurPosDifference = (firstTouch.position - secondTouch.position).magnitude;
+
+
+			zoomModifier = (firstTouch.deltaPosition - secondTouch.deltaPosition).magnitude * zoomModifierSpeed;
+
+			if (touchesPrevPosDifference > touchesCurPosDifference)
+				mainCamera.orthographicSize += zoomModifier;
+			if (touchesPrevPosDifference < touchesCurPosDifference)
+				mainCamera.orthographicSize -= zoomModifier;
+		}
+		mainCamera.orthographicSize = Mathf.Clamp (mainCamera.orthographicSize, 2f, 10f);
+	
+		text.text = "Camera size " + mainCamera.orthographicSize;
+	}
+}
---
+++ game-lazienki/Assets/Scripts/Touch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Touch : MonoBehaviour {
+
+	[SerializeField]
+	Text text;
+
+	Ray ray;
+
+	// Update is called once per frame
+	void Update () {
+
+		ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+		RaycastHit hit;
+		//if collide with sth => true, ray from the camera to infinity?
+		if (Physics.Raycast (ray, out hit)) { //Mathf.Infinity
+			
+			if (hit.transform.name == "PalacNaWodzie") {
+				text.text = hit.transform.name + " hit";
+			}
+			else if (hit.transform.name == "BialyDomek") {
+				text.text = hit.transform.name + " hit";
+			}
+		}
+	}
+}

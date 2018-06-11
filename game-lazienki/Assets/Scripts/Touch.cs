3e565bcbb32e4e3a71f2aab47cbea3f78b816914
kalagnie <37547184+kalagnie@users.noreply.github.com>
2018-06-11 14:57:20 +0200

    Moving map andsswitching scenes

--- game-lazienki/Assets/Scripts/Touch.cs
+++ game-lazienki/Assets/Scripts/Touch.cs
@@ -2,27 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Touch : MonoBehaviour {
 
-	[SerializeField]
-	Text text;
+	//[SerializeField]
+	//Text nameText;
+
+	//public Image namePanel;
 
 	Ray ray;
 
 	// Update is called once per frame
 	void Update () {
-
 		ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
 		RaycastHit hit;
 		//if collide with sth => true, ray from the camera to infinity?
-		if (Physics.Raycast (ray, out hit)) { //Mathf.Infinity
-			
-			if (hit.transform.name == "PalacNaWodzie") {
-				text.text = hit.transform.name + " hit";
+		if (Physics.Raycast (ray, out hit)) {
+
+			if (hit.transform.name == "PalacNaWodzie" || hit.transform.name == "BialyDomek") {
+				SceneManager.LoadScene (4); //4 => architecture
+				/*nameText.text = "Palac na Wodzie";
+				namePanel.transform.position = Input.GetTouch (0).position;
+				namePanel.gameObject.SetActive (true);
+				nameText.gameObject.SetActive (true);
+				*/
+			} else if (hit.transform.name == "Sobieski") {
+				SceneManager.LoadScene (3); //3=> conversation
 			}
-			else if (hit.transform.name == "BialyDomek") {
-				text.text = hit.transform.name + " hit";
+			else if (hit.transform.name == "Palac") {
+				SceneManager.LoadScene (2); //3=> conversation
 			}
 		}
 	}

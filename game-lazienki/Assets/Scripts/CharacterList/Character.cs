f0c5b1db889affacd48422f96dafeb09f6142c8d
kalagnie <37547184+kalagnie@users.noreply.github.com>
2018-06-13 14:20:58 +0200

    Start scene and inventory management

---
+++ game-lazienki/Assets/Scripts/CharacterList/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Character : MonoBehaviour {
+
+	public string characterName;
+	public int characterID;
+	public Sprite characterSprite;
+	public string characterInfo;
+}
